ddaf3445834505a9763f1bdd7ae6d8eb88410d4e
Isaac Marquez <iamr_30@hotmail.com>
2020-11-06 13:13:00 -0600

    Traduccion de alertas y botones
    Traducir las alertas, btotones y etiquetas del template

--- resources/views/profesores/table.blade.php
+++ resources/views/profesores/table.blade.php
@@ -25,7 +25,7 @@
                     <div class='btn-group'>
                         <a href="{{ route('profesores.show', [$profesores->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
                         <a href="{{ route('profesores.edit', [$profesores->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                        {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                        {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('¿Estas seguro de eliminar este profesor?')"]) !!}
                     </div>
                     {!! Form::close() !!}
                 </td>
